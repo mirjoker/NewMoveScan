253a2ad8118f96749552c418a0646ffb1578daaa
Liteng-Cpp <3110074895@qq.com>
2023-07-04 18:00:31 +0800

    function call graph

--- src/detect/detect6.rs
+++ src/detect/detect6.rs
@@ -3,7 +3,7 @@
 use std::collections::{BTreeMap, BTreeSet};
 
 use move_binary_format::{file_format::Visibility, internals::ModuleIndex, views::FunctionDefinitionView, access::ModuleAccess};
-use move_model::model::{QualifiedId, FunId};
+use move_model::model::{QualifiedId, FunId, ModuleId};
 use crate::move_ir::generate_bytecode::StacklessBytecodeGenerator;
 use petgraph::{graph::{Graph, DiGraph, NodeIndex}, Direction};
 use move_stackless_bytecode::stackless_bytecode::{
@@ -15,10 +15,15 @@ impl<'a> StacklessBytecodeGenerator<'a> {
     pub fn build_call_graph(&mut self) {
         let mut graph: Graph<QualifiedId<FunId>, ()> = DiGraph::new();
         let mut nodes: BTreeMap<QualifiedId<FunId>, NodeIndex> = BTreeMap::new();
-        for func_id in self.module_data.function_idx_to_id.values() {
+        let cm = self.module;
+        for func in cm.function_handles() {
+            let name = cm.identifier_at(func.name);
+            let symbol = self.symbol_pool.make(name.as_str());
+            let func_id = FunId::new(symbol);
+            let module_id = ModuleId::new(func.module.into_index());
             let qid = QualifiedId { 
                 module_id: self.module_data.id, 
-                id: *func_id,
+                id: func_id,
             };
             let node_idx = graph.add_node(qid);
             nodes.insert(qid, node_idx);
@@ -45,10 +50,6 @@ impl<'a> StacklessBytecodeGenerator<'a> {
             }).collect();
             
             for called_qid in called {
-                // TODO 构造所有函数的调用图，遇到的问题是rust不支持多次可变引用
-                // let dst_idx = nodes.entry(called_qid).or_insert_with(||{
-                //     graph.add_node(called_qid)
-                // });
                 let dst_idx = nodes.get(&called_qid);
                 if let Some(dst_idx) = dst_idx {
                     graph.add_edge(*src_idx, *dst_idx, ());
@@ -58,12 +59,17 @@ impl<'a> StacklessBytecodeGenerator<'a> {
         self.call_graph = graph;
         self.func_to_node = nodes;
     }
+
 }
 
 fn get_unused_functions<'a>(stbgr: &'a StacklessBytecodeGenerator) -> Vec<&'a QualifiedId<FunId>> {
     let mut unused_functions: Vec<&QualifiedId<FunId>> = vec![];
     for (fid, nid) in stbgr.func_to_node.iter() {
         // 调用边，即入边
+        if stbgr.module_data.function_data.get(&fid.id).is_none() {
+            // 理论上没有必要的操作，但是有脏东西，如aborted
+            continue;
+        }
         let neighbors = stbgr.call_graph.neighbors_directed(*nid, Direction::Incoming);
         if neighbors.into_iter().next().is_none() {
             unused_functions.push(fid);
