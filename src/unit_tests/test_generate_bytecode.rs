8e49976cdd76f31c536cca86f23824ad7c22184a
yule <1030845689@qq.com>
2023-08-02 15:19:42 +0800

    remove some old test codes

--- src/unit_tests/test_generate_bytecode.rs
+++ src/unit_tests/test_generate_bytecode.rs
@@ -7,14 +7,14 @@ use crate::utils::utils::compile_module;
 use move_stackless_bytecode::stackless_bytecode::Bytecode;
 use crate::move_ir::bytecode_display;
 
-#[test]
-fn test_generate_bytecode() {
-    let filename = PathBuf::from_str("testdata/examples_mv/aptos/infinite_loop.mv").unwrap();
-    let cm = compile_module(filename);
-    let mut bg = StacklessBytecodeGenerator::new(&cm);
-    bg.generate_function();
-    println!("{}", bg);
-    bg.get_control_flow_graph();
-    let cfg = bg.functions[0].cfg.as_ref().unwrap();
-    cfg.display();
-}
+// #[test]
+// fn test_generate_bytecode() {
+//     let filename = PathBuf::from_str("testdata/examples_mv/aptos/infinite_loop.mv").unwrap();
+//     let cm = compile_module(filename);
+//     let mut bg = StacklessBytecodeGenerator::new(&cm);
+//     bg.generate_function();
+//     println!("{}", bg);
+//     bg.get_control_flow_graph();
+//     let cfg = bg.functions[0].cfg.as_ref().unwrap();
+//     cfg.display();
+// }
