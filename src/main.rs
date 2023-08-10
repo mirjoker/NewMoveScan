e8e46f823d346a873bd1f5bf2f4a747f66d6b160
happytsing <wangleqing00@gmail.com>
2023-08-10 16:39:37 +0800

    release: printer 重构

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,25 @@
 #![allow(non_snake_case)]
-#[allow(unused_imports)]
 use MoveScanner::{
-    scanner::{
-        detector::Detector,
-        printer::Printer
-    },
     cli::parser::*,
+    scanner::{detector::Detector, printer::Printer},
 };
 
 use clap::Parser;
 
 fn main() {
-    // 命令行参数解析
     let cli = Cli::parse();
 
     match &cli.command {
-        Some(Commands::Printer)=> {
-            // todo
-            println!("Printer: todo");
-        },
+        Some(SubCommands::Printer) => {
+            // todo: 代码优化
+            let mut printer = Printer::new(cli.args);
+            printer.run();
+        }
         // 默认 Detector
-        _ =>{
+        _ => {
             let mut detector = Detector::new(cli.args);
             detector.run();
             detector.output_result();
         }
     }
-    
 }
